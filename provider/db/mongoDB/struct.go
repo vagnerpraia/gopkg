80c9cd635bf896d2fedd2adb3a6e91162a23ad0b
Vagner Praia <vagnerpraia@gmail.com>
2026-03-13 04:05:45 -0300

    env(mongodb): add functions

--- provider/db/mongoDB/struct.go
+++ provider/db/mongoDB/struct.go
@@ -9,7 +9,13 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func NewClient(ctx context.Context, clientOptions *ClientOptions) (*mongo.Client, *mongo.Database, error) {
+type MongoDB struct {
+	Client     *mongo.Client
+	Database   *mongo.Database
+	Collection *mongo.Collection
+}
+
+func NewMongoDB(ctx context.Context, clientOptions *ClientOptions) (*MongoDB, error) {
 
 	opts := options.Client().
 		ApplyURI(clientOptions.URI).
@@ -21,14 +27,60 @@ func NewClient(ctx context.Context, clientOptions *ClientOptions) (*mongo.Client
 
 	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
-		return nil, nil, fmt.Errorf("mongo.Connect failed: %w", err)
+		return nil, fmt.Errorf("mongo.Connect failed: %w", err)
 	}
 
 	if err := client.Ping(ctx, nil); err != nil {
-		return nil, nil, fmt.Errorf("client.Ping failed: %w", err)
+		return nil, fmt.Errorf("client.Ping failed: %w", err)
 	}
 
 	database := client.Database(clientOptions.DB)
 
-	return client, database, nil
+	mongoDB := MongoDB{
+		Client:   client,
+		Database: database,
+	}
+
+	return &mongoDB, nil
+}
+
+func (that *MongoDB) SetCollection(collection string) {
+
+	that.Database.Collection(collection)
+}
+
+func (that *MongoDB) Disconnect(ctx context.Context) error {
+
+	if err := that.Client.Disconnect(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (that *MongoDB) BulkWrite(
+	ctx context.Context,
+	writes []mongo.WriteModel,
+	retries int,
+	timeout time.Duration,
+	opts *options.BulkWriteOptions,
+) error {
+
+	var err error
+
+	for i := 0; i < retries; i++ {
+		ctxWrite, cancel := context.WithTimeout(ctx, timeout)
+
+		_, err = that.Collection.BulkWrite(ctxWrite, writes, opts)
+
+		cancel()
+
+		if err == nil {
+			return nil
+		}
+
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	return fmt.Errorf("bulkInsert failed: %w", err)
 }
